e42d7fa9e883c61a69803ae1c0ab47d061b5c2ed
Ovi <ovihossain7799@gmail.com>
2024-09-29 12:21:40 +0600

    Portfolio

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".banner-section-hadding2-colour", {
+    strings: ["a Frontend Designer", "a Developer", "a Freelancer","Ovi Hossain", "a Programmer"],
+    typeSpeed: 80,
+    backSpeed: 20,
+    looped: true
+});
+
+AOS.init();
+
